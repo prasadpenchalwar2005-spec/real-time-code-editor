7ca5e16668f7d9db5633f7530e00d891574f5e73
prasadpenchalwar2005-spec <prasadpenchalwar2005@gmail.com>
2026-09-16 11:41:05 +0530

    Initial commit

---
+++ Frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'monaco-editor/esm/vs/editor/editor.api.js': 'monaco-editor',
+      'monaco-editor/esm/vs/editor/editor.api': 'monaco-editor',
+    }
+  }
+})
